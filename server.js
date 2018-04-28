0f9dda3b826da87c14f87bc80d8bdd1a997f2312
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-04-28 13:25:48 -0400

    passportjs started

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const express = require('express');
+const server = express();
+const assert = require('assert');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+var router = express.Router(); // create router for REST routes
+
+// Server passport configuration
+server.use(require('express-session')({
+    secret: 'cb02bc565faa20f5b62a4cc2059204b1d803477f',
+    resave: false,
+    saveUninitialized: false
+}));
+server.use(passport.initialize());
+server.use(passport.session());
+
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// Register routes for the api
+server.use('/api', router);
+
+// Connect to mongodb first and then run the server. Otherwise, catch the error
+mongoose.connect('mongodb://localhost:27017/hackathon').then(() => {
+
+    server.get('/', (req, res) => {
+        res.send('Connected successfully to server');
+    });
+
+    server.listen(2008);
+
+}).catch(err => console.log(err));
